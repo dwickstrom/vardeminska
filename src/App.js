b257d4eff5d7352543c769207563a507b0c972c9
David Wickström <davidwickstrom@gmail.com>
2016-03-16 19:04:47 +0100

    Tweak things

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ export default class App extends Component {
         fuel_usage_rate: 0.7,
         ownership_length: parseFloat(36),
         yearly_value_decay: 16,
+        interest_rate: 2,
       }
     }
   }
@@ -47,6 +48,7 @@ export default class App extends Component {
     var money = parseFloat(this.state.inkopspris) +
                 parseFloat(this.state.service_repairs) +
                 parseFloat(this.getInsurances()) +
+                parseFloat(this.getInterest()) +
                 parseFloat(this.getTaxes()) +
                 parseFloat(this.getFuelCost()) -
                 parseFloat(this.getSalePrice())
@@ -54,39 +56,66 @@ export default class App extends Component {
   }
 
   getMilage() {
-    return (this.state.milage / 12) * this.state.ownership_length
+    return (this.state.milage / 12) * this.state.ownership_length || 0
   }
 
   getMilageCost() {
-    return this.getCosts() / this.getMilage()
+    return this.getCosts() / this.getMilage() || 0
   }
 
   getMonthlyCost() {
-    return this.getCosts() / this.state.ownership_length
+    return this.getCosts() / this.state.ownership_length || 0
   }
 
   getSalePrice() {
-    return this.state.inkopspris - this.calculateValueDecay()
+    return this.state.inkopspris - this.calculateValueDecay() || 0
   }
 
   getFuelCost() {
-    return this.getMilage() * this.state.fuel * this.state.fuel_usage_rate
+    return this.getMilage() * this.state.fuel * this.state.fuel_usage_rate || 0
   }
 
   getTaxes() {
-    return this.state.ownership_length * (this.state.tax / 12)
+    return this.state.ownership_length * (this.state.tax / 12) || 0
   }
 
   getInsurances() {
-    return this.state.ownership_length * this.state.insurance
+    return this.state.ownership_length * this.state.insurance || 0
   }
 
   getRepairs() {
-    return this.state.service_repairs
+    return this.state.service_repairs || 0
+  }
+
+  getInterest() {
+    const months = Array.apply(null, Array(this.state.ownership_length)).map(function (_, i) {return i;});
+    const getMonthlyInterest = (amount, interestRate) => {
+      const rate = interestRate / 100
+      return (amount * rate) / 12
+    }
+    const getMonthlyDownPaymentAmount = () => {
+      return this.calculateValueDecay() / this.state.ownership_length
+    }
+
+
+    const interest = months.reduce((acc, curr) => {
+      const monthlyInterest = getMonthlyInterest(acc.amount, this.state.interest_rate)
+       return {
+        interest: acc.interest + monthlyInterest,
+        amount: acc.amount - getMonthlyDownPaymentAmount()
+      }
+    }, {interest: 0, amount: this.state.inkopspris})
+
+    return round(interest.interest, 0)
+
   }
 
   clear() {
     cookie.remove('state')
+  }
+
+  saveState() {
+    cookie.save('state', JSON.stringify(this.state))
   }
 
   getChartData() {
@@ -125,11 +154,18 @@ export default class App extends Component {
         highlight: '#DEB887',
         label: 'Service & reparationer',
       },
+      {
+        method: 'getInterest',
+        color: 'lightgreen',
+        highlight: 'lightgreen',
+        label: 'Räntekostnader',
+      },
+
     ]
 
     return datapoints.map((obj, i) => {
       return {
-        value: round(this[obj.method](), 0),
+        value: round(this[obj.method](), 0) || 0,
         color: obj.color,
         highlight: obj.highlight,
         label: obj.label,
@@ -161,6 +197,7 @@ export default class App extends Component {
               <label>Försäkringspremie</label>
               <input className="form-control" name="insurance" onChange={::this.handleChange} type="number" value={this.state.insurance}></input>
             </div>
+            <button onClick={::this.saveState} className="btn btn-default">Spara kalkyl</button>
           </div>
           <div className="col-md-6">
             <div className="form-group">
@@ -187,6 +224,12 @@ export default class App extends Component {
               <label>Årlig värdeminskning ({this.state.yearly_value_decay} %)</label>
               <input type="range" min="0" max="100" step="0.1" name="yearly_value_decay" onChange={::this.handleChange} value={this.state.yearly_value_decay}></input>
             </div>
+
+            <div className="form-group">
+              <label>Ränta ({this.state.interest_rate} %)</label>
+              <input type="range" min="0" max="10" step="0.1" name="interest_rate" onChange={::this.handleChange} value={this.state.interest_rate}></input>
+            </div>
+
           </div>
           <hr />
         <div className="col-md-6" style={{paddingTop: 20}}>
@@ -195,45 +238,47 @@ export default class App extends Component {
               <tbody>
                 <tr>
                   <td>Försäljningspris</td>
-                  <td>{round(this.getSalePrice(), 0)} kr</td>
+                  <td>{round(this.getSalePrice(), 0) || 0} kr</td>
                 </tr>
 
                 <tr>
                   <td>Försäkringar</td>
-                  <td>{round(this.getInsurances(), 0)} kr</td>
+                  <td>{round(this.getInsurances(), 0) || 0} kr</td>
                 </tr>
 
                 <tr>
                   <td>Skatter</td>
-                  <td>{round(this.getTaxes(), 0)} kr</td>
+                  <td>{round(this.getTaxes(), 0) || 0} kr</td>
                 </tr>
 
                 <tr>
                   <td>Drivmedel</td>
-                  <td>{round(this.getFuelCost(), 0)} kr</td>
+                  <td>{round(this.getFuelCost(), 0) || 0} kr</td>
                 </tr>
 
                 <tr>
                   <td>Reparation, service & övrigt</td>
-                  <td>{round(this.state.service_repairs, 0)} kr</td>
+                  <td>{round(this.state.service_repairs, 0) || 0} kr</td>
                 </tr>
 
                 <tr>
                   <td>Värdeminskning</td>
-                  <td>{round(this.calculateValueDecay(), 0)} kr</td>
+                  <td>{round(this.calculateValueDecay(), 0) || 0} kr</td>
                 </tr>
 
                 <tr>
                   <td>Värdeminskning %</td>
-                  <td>{round((this.calculateValueDecay() / this.state.inkopspris) * 100, 2)}%</td>
+                  <td>{round((this.calculateValueDecay() / this.state.inkopspris) * 100, 2) || 0}%</td>
+                </tr>
+
+                <tr>
+                  <td>Räntekostnader</td>
+                  <td>{this.getInterest() || 0} kr</td>
                 </tr>
 
               </tbody>
             </table>
-
           </div>
-
-
 
         </div>
         <div className="col-md-4" style={{paddingTop: 20}}>
